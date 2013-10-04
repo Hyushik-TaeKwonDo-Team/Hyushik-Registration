1aa20fee4a8925708f2fdb84a482bf68619d0f57
Craig Felton <feltocraig@gmail.com>
2013-10-04 11:16:15 -0400

    Updated signup.php

--- code/Hyushik_Registration/signup.php
+++ code/Hyushik_Registration/signup.php
@@ -2,9 +2,18 @@
 <body>
 
 <?php 
-/* The HTML document needs to have the name field defined for _POST*/
+/* The HTML document needs to have the name field defined for _POST
+
+fputcsv
+
+*/
 $name =  $_POST["name"];
 $email = $_POST["email"];
+
+$list = array($name, $email);
+$fp = fopen('registration.csv', 'a');
+fputcsv($fp, $list);
+fclose($fp);
 
 echo "Name: $name <br>";
 echo "Email: $email";
