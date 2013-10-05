8f157f6d67ef8c0f9fdcdf7ea63f77ef807777a6
Craig Felton <feltocraig@gmail.com>
2013-10-05 00:14:30 -0400

    Updated signup.php and added all the input fields

--- code/Hyushik_Registration/signup.php
+++ code/Hyushik_Registration/signup.php
@@ -3,7 +3,7 @@
 
 <?php 
 
-/*Participant Info*/
+/* Participant Info */
 $name =  $_POST["name"];
 $email = $_POST["email"];
 $address = $_POST["address"];
@@ -11,16 +11,43 @@ $city = $_POST["city"];
 $state = $_POST["state"];
 $zip = $_POST["zip"];
 $phone = $_POST["phone"];
-$gender = $_POST["gender"];
+$gender = $_POST["gender"]; //Will throw an error if not set
+
+/* School */
+$instructor =  $_POST["instructor"];
+$schoolname =  $_POST["schoolname"];
+$schooladdress =  $_POST["schooladdress"];
+$schoolcity =  $_POST["schoolcity"];
+$schoolstate =  $_POST["schoolstate"];
+$schoolzip =  $_POST["schoolzip"];
+$schoolphone =  $_POST["schoolphone"];
+$schoolemail =  $_POST["schoolemail"];
+
+/* Event */
+$rank =  $_POST["rank"];
+$age =  $_POST["age"];
+$weight =  $_POST["weight"];
+if(isset($_POST['weapons'])){ $weapons = "Yes"; }
+else{ $weapons = "No"; }
+if(isset($_POST['breaking'])){ $breaking = "Yes"; }
+else{ $breaking = "No"; }
+if(isset($_POST['sparring'])){ $sparring = "Yes"; }
+else{ $sparring = "No"; }
+if(isset($_POST['point'])){ $point = "Yes"; }
+else{ $point = "No"; }
+if(isset($_POST['olympic'])){ $olympic = "Yes"; }
+else{ $olympic = "No"; }
+$boards =  $_POST["boards"];
 
 
-$list = array($name, $email);
+$list = array($name, $email, $address, $city, $state, $zip, $phone, $gender, $instructor, $schoolname, $schooladdress, $schoolcity, $schoolstate, $schoolzip, $schoolphone, $schoolemail, $rank, $age, $weight, $weapons, $breaking, $sparring, $point, $olympic, $boards );
 $fp = fopen('registration.csv', 'a');
 fputcsv($fp, $list);
 fclose($fp);
 
 echo "Name: $name <br>";
-echo "Email: $email";
+echo "Email: $email <br>";
+echo "Weapons: $weapons";
 
 ?>
 
