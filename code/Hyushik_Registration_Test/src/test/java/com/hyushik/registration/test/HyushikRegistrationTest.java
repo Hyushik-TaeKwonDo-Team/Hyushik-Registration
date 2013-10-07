d62d6627966c6e2bcefcd6a760eecf83b8e386b6
Patrick McAfee <McAfee@agentsmith.student.rit.edu>
2013-10-06 22:04:29 -0400

    Testing file writes now exist

--- code/Hyushik_Registration_Test/src/test/java/com/hyushik/registration/test/HyushikRegistrationTest.java
+++ code/Hyushik_Registration_Test/src/test/java/com/hyushik/registration/test/HyushikRegistrationTest.java
@@ -21,12 +21,15 @@ import java.util.Properties;
 import java.io.FileInputStream;
 import java.io.IOException;
 import au.com.bytecode.opencsv.CSVReader;
+import com.hyushik.registration.test.entities.Participant;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -43,9 +46,14 @@ public class HyushikRegistrationTest {
     private String csvName;
     private String csvPath;
     private WebDriver driver;
+    
+    private Participant part1 = new Participant("Test Participant", 
+            "test@test.com", "5 Nowhere Lane", "Bangor", 
+            "Maine", "12345", "555-55-5555", Participant.Gender.MALE, 
+            "Test Instructor", "Test School", 23, 195 );
 
-    @Before
-    public void openBrowser() {
+    
+    private void setUpVars(){
         FileInputStream fizban;
         try {
             fizban = new FileInputStream("webserver.properties");
@@ -58,12 +66,23 @@ public class HyushikRegistrationTest {
             return;
         }
 
-        baseUrl = props.getProperty(PropertiesNames.weburl);
-        csvFolder = props.getProperty(PropertiesNames.filepathToWebRoot);
-        csvName = props.getProperty(PropertiesNames.csvFileName);
+        String csvFolder = props.getProperty(PropertiesNames.filepathToWebRoot);
+        String csvName = props.getProperty(PropertiesNames.csvFileName);
         csvPath = csvFolder + csvName;
+        
+        baseUrl = props.getProperty(PropertiesNames.weburl);
+    }
+    
+    private void openBrowser(){
         driver = new FirefoxDriver();
         driver.get(baseUrl);
+    }
+    
+    @Before
+    public void setUp() {
+        setUpVars();
+        deleteCSVFile();
+        openBrowser();
     }
 
     @Test
@@ -75,15 +94,57 @@ public class HyushikRegistrationTest {
     @Test
     public void CSVFileIsBeingWritten() {
         WebElement myDynamicElement = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.id("name")));
-        
-        submitRegistration("Test", "Test01@test.com");
+        submitRegistration(part1);
         assertTrue( "File "+csvPath+" does not exist.", fileExists(csvPath));
         
     }
+    
+    @Test
+    public void oneRegistrationIsSucessful() {
+        WebElement myDynamicElement = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.id("name")));
+        submitRegistration(part1);
+        List<Participant> parts = new ArrayList<Participant>(Arrays.asList(part1));
+        validateParticipantsInCSVFile(parts); 
+    }
+    
+    private void validateParticipantsInCSVFile(List<Participant> participants){
+        
+        List<String[]> results = new ArrayList<String[]>();
+        try {
+            CSVReader reader = new CSVReader(new FileReader(csvPath));
+            results =reader.readAll();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(HyushikRegistrationTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail(ex.getMessage());
+        }catch (IOException ex) {
+            Logger.getLogger(HyushikRegistrationTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        for(String[] inputLine : results){
+            validateParticipantsInCSVFile(part1, inputLine);
+        }
+    }
+    
+    private void validateParticipantsInCSVFile(Participant part, String[] inputLine){
+        assertEquals(part.getName(), inputLine[0]);
+    }
+    
 
     @After
-    public void closeBrowserWindows() {
+    public void tearDown() {
+        closeBrowsers();
+        deleteCSVFile();
+    }
+    
+    private void closeBrowsers() {
         driver.quit(); //DON'T USE "driver.close", it fails in QA
+    }
+    
+    private void deleteCSVFile() {
+        File csv = new File(csvPath);
+        if (csv.exists()){
+            csv.delete();
+        }
     }
     
     private boolean fileExists(String filePath){
@@ -91,9 +152,27 @@ public class HyushikRegistrationTest {
         return csv.exists();
     }
     
-    private void submitRegistration(String name, String email){
-        driver.findElement(By.id("name")).sendKeys(name);
-        driver.findElement(By.id("email")).sendKeys(email);
+    private void submitRegistration(Participant part){
+        driver.findElement(By.id("name")).sendKeys(part.getName());
+        driver.findElement(By.id("email")).sendKeys(part.getEmail());
+        driver.findElement(By.id("address")).sendKeys(part.getAddress());
+        driver.findElement(By.id("city")).sendKeys(part.getCity());
+        driver.findElement(By.id("state")).sendKeys(part.getState());
+        driver.findElement(By.id("phone")).sendKeys(part.getPhone());
+        driver.findElement(By.id("zip")).sendKeys(part.getZip());
+        
+        if (part.getGender()==Participant.Gender.MALE){
+            driver.findElement(By.id("male")).click();
+        }else if(part.getGender()==Participant.Gender.FEMALE){
+            driver.findElement(By.id("female")).click();
+        }
+        
+        driver.findElement(By.id("instructor")).sendKeys(part.getInstructor());
+        driver.findElement(By.id("schoolname")).sendKeys(part.getSchool());
+
+        driver.findElement(By.id("age")).sendKeys(Integer.toString(part.getAge()));
+        driver.findElement(By.id("weight")).sendKeys(Integer.toString(part.getWeight()));
+        
         driver.findElement(By.id("submitButton")).click();
     }
 
@@ -125,7 +204,7 @@ public class HyushikRegistrationTest {
 
     }
 
-    public void compareCSVLine(String[] expected, String[] received) {
+    private void compareCSVLine(String[] expected, String[] received) {
         assertTrue("Expect CSV line "
                 + expected.toString() + " "
                 + "does not match read line "
